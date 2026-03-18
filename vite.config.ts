5686e2e884ee8bd512a413b6b4dff858143aea0c
Sebastien Leib <sebastien.leib1@gmail.com>
2026-03-18 17:45:08 +0800

    Fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
+  base: '/wave/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
